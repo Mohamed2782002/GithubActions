54049f6de90c9ff43d9eb2f6a6012a3a659e02e7
Mohamed2782002 <142643021+Mohamed2782002@users.noreply.github.com>
2025-03-30 20:45:17 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -26,4 +26,58 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
+
+	[TestMethod]
+	public void TestPower_Mousa()
+	{
+		Assert.AreEqual(8.0, Program.Power("2", "3"));
+		Assert.AreEqual(1.0, Program.Power("5", "0"));
+		Assert.AreEqual(0.0, Program.Power("0", "5"));
+	}
+	[TestMethod]
+	public void TestSubtract_Mousa()
+        {
+            // Assuming you have implemented a Subtract method:
+            // 3 - 2 should equal 1
+            double expected = 1.0;
+            double actual = Program.Subtract("3", "2");
+            Assert.AreEqual(expected, actual);
+
+            // 5 - 5 should equal 0
+            expected = 0.0;
+            actual = Program.Subtract("5", "5");
+            Assert.AreEqual(expected, actual);
+
+            // 3 - 5 should equal -2
+            expected = -2.0;
+            actual = Program.Subtract("3", "5");
+            Assert.AreEqual(expected, actual);
+        }
+		[TestMethod]
+        public void TestDivide_Mousa()
+        {
+            // 6 / 3 should equal 2
+            double expected = 2.0;
+            double actual = Program.Divide("6", "3");
+            Assert.AreEqual(expected, actual);
+
+            // 10 / 2 should equal 5
+            expected = 5.0;
+            actual = Program.Divide("10", "2");
+            Assert.AreEqual(expected, actual);
+        }
+		public void TestMultiply_Mousa()
+        {
+            // 2 * 3 should equal 6
+            double expected = 6.0;
+            double actual = Program.Multiply("2", "3");
+            Assert.AreEqual(expected, actual);
+
+            // 0 * 5 should equal 0
+            expected = 0.0;
+            actual = Program.Multiply("0", "5");
+            Assert.AreEqual(expected, actual);
+        }
+
+
 }
